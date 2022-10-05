6cdb659743b726de485dc3a7e786e92357570fea
safawat19 <safawat.hossain@byslglobal.com>
2022-10-05 19:24:28 +0600

    register and login done

--- Social/client/pages/login.js
+++ Social/client/pages/login.js
@@ -7,11 +7,10 @@ import axios from 'axios'
 import { useRouter } from 'next/router'
 import Link from 'next/link';
 
-const register = () => {
-    const [name, setName] = useState("")
+const login = () => {
+
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const [answer, setAnswer] = useState("")
     const [loading, setLoading] = useState(false)
     const router = useRouter()
 
@@ -19,22 +18,22 @@ const register = () => {
         e.preventDefault();
         try {
             setLoading(true)
-            const { data } = await axios.post(`${process.env.NEXT_PUBLIC_API}/register`, {
-                name, email, password, answer
+            const { data } = await axios.post(`${process.env.NEXT_PUBLIC_API}/login`, {
+                email, password
             })
             setLoading(false)
-            toast.success("User registered successfully")
-            router.push('./login')
+            console.log(data)
+            toast.success("User login successfully")
+            // router.push('./login')
         } catch (error) {
-            toast.error(error.response.data)
+            toast.error("Try agin")
         }
     }
-
     return (
         <Layout>
             <div className='row d-flex justify-content-center align-items-center'>
                 <div className='col-md-8'>
-                    <h1 className='pt-4 mb-3'>Register Now </h1>
+                    <h1 className='pt-4 mb-3'>Login Now </h1>
                     <form >
                         <ToastContainer
                             position="top-right"
@@ -48,10 +47,6 @@ const register = () => {
                             pauseOnHover
                         />
                         <div className="mb-3">
-                            <label className="form-label">Name</label>
-                            <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="form-control" aria-describedby="emailHelp" />
-                        </div>
-                        <div className="mb-3">
                             <label className="form-label">Email address</label>
                             <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" aria-describedby="emailHelp" />
                         </div>
@@ -59,23 +54,13 @@ const register = () => {
                             <label className="form-label">Password</label>
                             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" />
                         </div>
-
-                        <select className="form-select mb-2" aria-label="Default select example">
-                            <option defaultValue>Select Security Question</option>
-                            <option value="1">Enter your best friend name</option>
-                            <option value="2">Enter your best teacher name</option>
-                            <option value="3">Enter your best first school name</option>
-                        </select>
-                        <div className="mb-3">
-                            <input type="text" value={answer} onChange={(e) => setAnswer(e.target.value)} className="form-control" placeholder='enter answer' aria-describedby="emailHelp" />
-                        </div>
                         <div className='d-flex flex-row'>
-                            <button type="submit" disabled={!name || !email || !password || !answer} onClick={handleSubmit} className="btn btn-primary">{loading ? (<>
+                            <button type="submit" disabled={!email || !password} onClick={handleSubmit} className="btn btn-primary">{loading ? (<>
                                 <span>Loading</span> <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                            </>) : 'Register'}
+                            </>) : 'Login'}
 
                             </button>
-                            <p className='m-3'> Are You Registered ?<Link href="./login">Go Login </Link></p>
+                            <p className='m-3'> Are You New ? <Link href="./register">Go to registration </Link></p>
                         </div>
                     </form>
                 </div>
@@ -84,4 +69,4 @@ const register = () => {
     )
 }
 
-export default register
+export default login
