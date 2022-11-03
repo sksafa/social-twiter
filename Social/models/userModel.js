7705c792dbc22d4669de8ea7b3e2a2b4d0812f44
safawat19 <safawat.hossain@byslglobal.com>
2022-11-03 16:28:09 +0600

    updated user profile

--- Social/models/userModel.js
+++ Social/models/userModel.js
@@ -1,39 +1,43 @@
 import mongoose from "mongoose";
-const {Schema} = mongoose
+const { Schema } = mongoose;
 
-const userSchema = new Schema({
-    name:{
-        type:String,
-        trim:true,
-        required:true
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
     },
-    email:{
-        type:String,
-        trim:true,
-        required:true,
-        unique:true
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 64,
+    },
+    answer: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    about: {},
+    image: {
+      url: String,
+      public_id: String,
+    },
+    following: [{ type: Schema.ObjectId, ref: "User" }],
+    followers: [{ type: Schema.ObjectId, ref: "User" }],
+  },
+  { timestamps: true }
+);
 
-    },
-    password:{
-        type:String,
-        required:true,
-        min:6,
-        max:64
-    },
-    answer:{
-        type:String,
-        required:true,
-    },
-    about:{},
-    photo:String,
-    following:[{
-        type:Schema.ObjectId,
-        ref:'User'
-    }],
-    followers:[{
-        type:Schema.ObjectId,
-        ref:'User'
-    }]
-}, {timestamps:true})
-
-export default mongoose.model('User',userSchema)
+export default mongoose.model("User", userSchema);
